018dcd4f5f8f241b22bf19f417386ab28e774285
Alexander Martynov <alexmart89@mail.ru>
2018-04-26 21:42:24 +0300

    add input client parameters

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,54 @@
 #include "Client.h"
 
-int main(int /*argc*/, char** /*argv*/)
+#include <getopt.h>
+#include <iostream>
+
+int main(int argc, char** argv)
 {
+
+    const option options[] {
+        { "proto", required_argument, NULL, 't' },
+        { "port", required_argument, NULL, 'p' },
+        { NULL, no_argument, NULL, 0 }        
+    };
+    
+    std::string protocol = {};
+    int port = 0;
+    int opt = getopt_long(argc, argv, "t:p:", options, nullptr);
+    while(opt != -1)
+    {
+        switch(opt)
+        {
+            case 't':
+            {
+                protocol =  optarg;
+                break;
+            }
+            case 'p':
+            {
+                port = std::atoi(optarg);
+                std::cout << "Port: " << port << std::endl;
+                break;
+            }
+            default: /* ? */
+            {
+                std::cout << "Usage " << argv[0] << " [-t TCP/UDP] [-p port]" << std::endl;
+                break;
+            }
+        }
+
+        opt = getopt_long(argc, argv, "t:p:", options, nullptr);
+    }
+
+    if(protocol.empty() || port == 0)
+    {
+        std::cerr << "Usage " << argv[0] << " [-t TCP/UDP] [-p port]" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Protocol: " << protocol << " port: " << port << std::endl;
+
+  
     Client client;
     int result = client.exec();
     return result;
